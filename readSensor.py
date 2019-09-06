197e1aeccbe54607357cc81a76d1902445fbb333
Daniel Keats <dkeats12@gmail.com>
2019-09-06 12:34:14 -0500

    Added example

--- readSensor.py
+++ readSensor.py
@@ -1,1 +1,15 @@
 #Purpose is to read all sensor data and make it readable
+
+#uh this will all change later I just needed a frame work
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+        print('Button Pressed')
+        time.sleep(0.2)
