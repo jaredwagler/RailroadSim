c18ea9e38a87ae3753ec332c564505c0c4587ae9
Daniel Keats <dkeats12@gmail.com>
2019-09-07 23:20:10 -0500

    Cleaned up

--- readSensor.py
+++ readSensor.py
@@ -5,6 +5,8 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM) #now we can use easy numbers
+
 #We are going to declare all of our items and their GPIO pins
 directionF = 12
 directionB = 16
@@ -14,65 +16,53 @@ encoder2CLK = 24
 encoder2DT = 25
 horn = 20
 bell = 21
-t0 = 4
-t1 = 17
-t2 = 27
-t3 = 22
-t4 = 5
-t5 = 6
-t6 = 13
-t7 = 19
-t8 = 26
+throttle_0 = 4
+throttle_1 = 17
+throttle_2 = 27
+throttle_3 = 22
+throttle_4 = 5
+throttle_5 = 6
+throttle_6 = 13
+throttle_7 = 19
+throttle_8 = 26
 
-GPIO.setmode(GPIO.BCM) #really don't know what this does but it's here
+sensors = (directionF, directionB, encoder1CLK, encoder1DT, encoder2CLK, encoder2DT, horn, bell, throttle_0, throttle_1, throttle_2, throttle_3, throttle_4, throttle_5, throttle_6, throttle_7, throttle_8)
+throttle = (throttle_0, throttle_1, throttle_2, throttle_3, throttle_4, throttle_5, throttle_6, throttle_7, throttle_8)
+direction = (directionF, directionB)
 
 #Just gonna set everything to be inputs
-GPIO.setup(directionF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(directionF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(encoder1CLK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(encoder1DT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(encoder2CLK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(encoder2DT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(horn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(bell, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(t8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for pin in sensors:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 lastThrottlePosition = 0
+lastDirection = None
 
-while True: #main shit
+while True: #main shit, should make it callable at a later point
+    hornState = GPIO.input(horn)
+    bellState = GPIO.input(bell)
+    
     currentThrottlePosition = getThrottlePosition()
-    if currentThrottlePosition == 9: #maintains throttle value when inbetween positions
-        throttle = lastThrottlePosition
+    if currentThrottlePosition == None: #maintains throttle value when inbetween positions
+        throttleVal = lastThrottlePosition
     else:
         lastThrottlePosition = currentThrottlePosition
-
-def getThrottlePosition():
-    if GPIO.input(t0) == False:
-        state = 0
-    if GPIO.input(t1) == False:
-        state = 1
-    if GPIO.input(t2) == False:
-        state = 2
-    if GPIO.input(t3) == False:
-        state = 3
-    if GPIO.input(t4) == False:
-        state = 4
-    if GPIO.input(t5) == False:
-        state = 5
-    if GPIO.input(t6) == False:
-        state = 6
-    if GPIO.input(t7) == False:
-        state = 7
-    if GPIO.input(t8) == False:
-        state = 8
+        throttleVal = currentThrottlePosition
+    
+    currentDirection = getDirection() #gets direction else none, 0 = forward, 1 = backwards, None = neutral
+    if currentDirection == None:
+        directionVal = lastDirection
     else:
-        state = 9
-    return state
+        lastDirection = currentDirection
+        directionVal = currentDirection
+
+def getThrottlePosition(): #returns the position of the throtle else None
+    for position, pin in enumerate(throttle):
+        if not GPIO.input(pin):
+            return position
+    return None #inbetween throttle positions
+
+def getDirection():
+    for position, pin in enumerate(direction):
+        if not GPIO.input(pin):
+            return position
+    return None #train is in netural
