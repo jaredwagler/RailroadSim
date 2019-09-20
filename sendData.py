9ffb38907101ebfaed87db8504ab5b210533e6aa
Jared Wagler <jared.wagler@gmail.com>
2019-09-20 11:05:40 -0500

    Added command line output for train direction and throttle position

--- sendData.py
+++ sendData.py
@@ -5,8 +5,19 @@
 ESU_PORT = 15471
 ESU_RCV_SZ = 1024
    
-def esu_send(cmdNum):
+def esu_send(cmdNum,cmdVal):
 	#code to send to esubridge
+	if cmdNum == 29: #direction
+		if cmdVal == 0:
+			print ('Train is going forwards.')
+		elif cmdVal == 1:
+			print ('Train is going backwards.')
+		else:
+			print ('Train is idling.')
+
+	return
 
 def throttle_send(throttlePos)
 	#code to send to esubridge
+	print ('Train is traveling at speed %d.' % (throttlePos))
+	return
