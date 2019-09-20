176f20c66cf4749afa915100b6711c675a0f6a61
Jared Wagler <jared.wagler@gmail.com>
2019-09-20 11:37:07 -0500

    Added more commands to sendData.py

--- sendData.py
+++ sendData.py
@@ -7,17 +7,31 @@ ESU_RCV_SZ = 1024
    
 def esu_send(cmdNum,cmdVal):
 	#code to send to esubridge
-	if cmdNum == 29: #direction
+	if cmdNum == 1: #bell
+		if cmdVal == 1:
+			print ('Bell is ringing.')
+		elif cmdVal == 0:
+			print ('Bell is off.')
+	if cmdNum == 2: #horn
+		if cmdVal == 1:
+			print ('Horn is blowing.')
+		elif cmdVal == 0:
+			print ('Horn is off.')
+	if cmdNum == 15: #e-brake
+		if cmdVal == 1:
+			print ('Train is emergency braking.')
+		elif cmdVal == 0:
+			print ('Emergency brake not pressed.')
+	elif cmdNum == 29: #direction
 		if cmdVal == 0:
 			print ('Train is going forwards.')
 		elif cmdVal == 1:
 			print ('Train is going backwards.')
 		else:
 			print ('Train is idling.')
-
 	return
 
-def throttle_send(throttlePos)
+def throttle_send(throttlePos):
 	#code to send to esubridge
 	print ('Train is traveling at speed %d.' % (throttlePos))
 	return
