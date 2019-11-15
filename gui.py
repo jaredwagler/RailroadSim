96a5e4fd6cc9c3f8fe9656c5fc5db26c437dabbd
Jared Wagler <jared.wagler@gmail.com>
2019-11-15 11:38:20 -0600

    Added comments to easily read code later

--- gui.py
+++ gui.py
@@ -1,37 +1,40 @@
+# To display a gui to the user to get easy input
+# Created by: Jared Wagler
+# Updated on: 11/15/2019
+
 from tkinter import *
 import os
 
 class userGui:
     def __init__(self):
 
-        self.serverIp = ""
-        self.locoAddress = ""
-        ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))
-        INFO = os.path.join(ROOT_FOLDER, 'info.txt')
-        IMAGE = os.path.join(ROOT_FOLDER, 'choochoo.gif')
+        self.serverIp = ""                                          # initialize the IP as blank to start
+        self.locoAddress = ""                                       # initialize the locomotive address as blank to start
+        ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))    # gets the root folder the program is in
+        INFO = os.path.join(ROOT_FOLDER, 'info.txt')                # grabs the info file for us
 
         window = Tk()
 
-        def manualClicked():
+        def manualClicked():            # user clicked manual input
             global serverIp
             global locoAddress
-            serverIp = txt1.get()
-            locoAddress = txt2.get()
-            f = open(INFO,'w')
-            f.write(serverIp + '\n')
+            serverIp = txt1.get()       # grabs IP from prompt
+            locoAddress = txt2.get()    # grabs locomotive address from prompt
+            f = open(INFO,'w')          # opens info file in write mode
+            f.write(serverIp + '\n')    # writes user input into the file
             f.write(locoAddress)
 
-            if ((serverIp != "") and (locoAddress != "")):
+            if ((serverIp != "") and (locoAddress != "")):  # only closes gui if user entered info
                 f.close()
                 window.destroy()
         
-        def fileClicked():
+        def fileClicked():                          # user clicked previous input
             global serverIp
             global locoAddress
-            f = open(INFO,'r')
-            serverIp = f.readline()
+            f = open(INFO,'r')                      # opens info file in read mode
+            serverIp = f.readline()                 # grabs IP from file
             serverIp = serverIp.rstrip('\n')
-            locoAddress = f.readline()
+            locoAddress = f.readline()              # grabs locomotive address from file
             locoAddress = locoAddress.rstrip('\n')
             f.close()
             window.destroy()
@@ -40,40 +43,43 @@ class userGui:
         
         window.geometry('310x130')
 
-        prompt1 = Label(window, text="Enter Server IP: ")
+        prompt1 = Label(window, text="Enter Server IP: ")                   # IP prompt
         prompt1.grid(column=1, row=1)
 
-        prompt2 = Label(window, text="Enter Locomotive Address: ")
+        prompt2 = Label(window, text="Enter Locomotive Address: ")          # locomotive address prompt
         prompt2.grid(column=1, row=2)
         
-        txt1 = Entry(window,width=15)
+        txt1 = Entry(window,width=15)                                       # text entry for IP
         txt1.grid(column=2, row=1)
-        txt1.focus_set()
+        txt1.focus_set()                                                    # sets cursor to start at this prompt
 
-        txt2 = Entry(window,width=15)
+        txt2 = Entry(window,width=15)                                       # text entry for locomotive address
         txt2.grid(column=2, row=2)
         
-        btn1 = Button(window, text="Manual Input", command=manualClicked) 
+        btn1 = Button(window, text="Manual Input", command=manualClicked)   # manual input button
         btn1.grid(column=2, row=3)
 
-        btn2 = Button(window, text="Previous Input", command=fileClicked) 
+        btn2 = Button(window, text="Previous Input", command=fileClicked)   # file input button
         btn2.grid(column=1, row=3)
 
-        f = open(INFO,'r')
-        tempIP = f.readline()
+        f = open(INFO,'r')              # opens info file in read mode
+        tempIP = f.readline()           # gets previous IP to display on gui
         tempIP= tempIP.rstrip('\n')
-        tempAdr = f.readline()
+        tempAdr = f.readline()          # gets previous locomotive address to display on gui
         tempAdr = tempAdr.rstrip('\n')
 
-        lastIP = Label(window, text="Previous IP Address: " + tempIP)
+        lastIP = Label(window, text="Previous IP Address: " + tempIP)           # displays previously used IP address
         lastIP.grid(column=1, row=4, pady=(10,0))
 
-        lastAdr = Label(window, text="Previous Locomotive Address: " + tempAdr)
+        lastAdr = Label(window, text="Previous Locomotive Address: " + tempAdr) # displays previously used locomotive address
         lastAdr.grid(column=1, row=5)
 
         window.mainloop()
 
-    def returnValues(self):
+    def returnValues(self):             # returns IP and locomotive address to main part of code
         global serverIp
         global locoAddress
         return serverIp, locoAddress
+
+# for testing the gui without having to open readSensor.py
+# guiInput = userGui()
